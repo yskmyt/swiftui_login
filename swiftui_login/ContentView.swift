fa1c1b41369fa3dd39549817189b6f9ce8fadda6
Yusuke Miyata <ysk379.skrmj@gmail.com>
2021-05-16 15:23:38 +0900

    Initial Commit

---
+++ swiftui_login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_login
+//
+//  Created by yusuke on 2021/05/16.
+//  Copyright © 2021 Yusuke Miyata. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
